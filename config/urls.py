e84b3278fa3a4678b0d1e9729994707bc898f6a4
LukeI50 2 <180131099+LukeI50@users.noreply.github.com>
2024-11-27 23:59:56 +0000

    Add: urls.py to app with path url

--- config/urls.py
+++ config/urls.py
@@ -16,9 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from project_board.views import projectsList
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('projects/', projectsList, name='projects')
+    path('projects/', include('project_board.urls'), name='projects')
 ]
