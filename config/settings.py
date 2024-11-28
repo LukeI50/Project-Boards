3af2190e7a122ca598f8da9d4a1c1857b7c46d2b
LukeI50 2 <180131099+LukeI50@users.noreply.github.com>
2024-11-28 14:27:55 +0000

    Add: migrations to database, register models with admin, and create superuser

--- config/settings.py
+++ config/settings.py
@@ -112,6 +112,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://127.0.0.1', 'https://*herokuapp.com'
+]
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
