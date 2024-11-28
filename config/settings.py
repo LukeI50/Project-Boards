37041a3b1f2ef557a072a4973915a61800c7cb17
LukeI50 2 <180131099+LukeI50@users.noreply.github.com>
2024-11-28 00:58:37 +0000

    Add: Project model to models.py in project_board

--- config/settings.py
+++ config/settings.py
@@ -33,7 +33,7 @@ if os.path.isfile('env.py'):
 else:
     DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
--- project_board/models.py
+++ project_board/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+STATUS = ((0, 'Backlog'), (1, 'Todo'), (2, 'In Progress'), (3, 'Done'))
 
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField()
+    slug = models.SlugField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='project_boards')
+    description = models.TextField()
+    date_created = models.DateField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+    last_updated_by = models.ForeignKey(User, name='last_update_by')
+    authorised_editors = models.ForeignKey(User, related_name='editors')
+    todos = models.ForeignKey(Todos)
+
+
