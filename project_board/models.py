3fec0c62c01b24653c5b6b0c06333c1d2c2b43b0
LukeI50 2 <180131099+LukeI50@users.noreply.github.com>
2024-11-28 19:23:50 +0000

    Add: many to many table explanation comment in the models.py section

--- project_board/models.py
+++ project_board/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Permission
 from django.contrib.auth import get_user_model
 
 
@@ -20,8 +20,10 @@ class Project(models.Model):
     description = models.TextField()
     date_created = models.DateField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
-    last_updated_by = models.OneToOneField(User, on_delete=models.SET(dud_user), blank=True, name='last_updated_by')
-    allowed_editors = models.ForeignKey(User, on_delete=models.SET(dud_user), blank=True, null=True, related_name='authorised_editor')
+    last_updated_by = models.OneToOneField(User, on_delete=models.SET(dud_user), blank=True, null=True, name='last_updated_by')
+    # A Many to Many Field creates a sub table that links the relevant objects in their own table.
+    # Technically this table is called project_board_authorised_editors
+    authorised_editors = models.ManyToManyField(User, blank=True, related_name='authorised_editor')
 
 
 class Task(models.Model):
