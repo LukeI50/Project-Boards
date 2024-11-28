ad874e9f89c2472547b176ffb69c6d2e1a6760f8
LukeI50 2 <180131099+LukeI50@users.noreply.github.com>
2024-11-28 00:59:09 +0000

    Add: Todo model to models.py in project_board app

--- project_board/models.py
+++ project_board/models.py
@@ -5,6 +5,24 @@ from django.contrib.auth.models import User
 STATUS = ((0, 'Backlog'), (1, 'Todo'), (2, 'In Progress'), (3, 'Done'))
 
 # Create your models here.
+class Todos(models.Model):
+    class TaskStatus(models.IntegerChoices):
+        BACKLOG = 0, "Backlog"
+        TODO = 1, "Todo"
+        IN_PROGRESS = 2, "In Progress"
+        DONE = 3, "Done"
+
+    task_status = models.PositiveSmallIntegerField(choices=TaskStatus)
+
+    title = models.CharField()
+    slug = models.SlugField()
+    status = models.TextChoices()
+    date_created = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, name='created_by')
+    last_updated = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+
+
 class Project(models.Model):
     title = models.CharField()
     slug = models.SlugField()
