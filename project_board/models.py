27151a417d2e17b8eab97d5acbc0b90165d17a70
LukeI50 2 <180131099+LukeI50@users.noreply.github.com>
2024-11-28 01:44:30 +0000

    Add: dud user function for user deletions, and fix task status field choices call

--- project_board/models.py
+++ project_board/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.conf import settings
+from django.contrib.auth import get_user_model
 
 
 STATUS = ((0, 'Backlog'), (1, 'Todo'), (2, 'In Progress'), (3, 'Done'))
+
+def dud_user():
+    return get_user_model().objects.get_or_create(username = 'deleted')[0]
+
 
 # Create your models here.
 class Task(models.Model):
@@ -12,12 +18,12 @@ class Task(models.Model):
         IN_PROGRESS = 2, "In Progress"
         DONE = 3, "Done"
 
-    status = models.PositiveSmallIntegerField(choices=TaskStatus)
+    status = models.PositiveSmallIntegerField(choices=TaskStatus.choices, default=TaskStatus.BACKLOG)
 
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200)
-    date_created = models.DateTimeField(auto_now_add=True)
-    created_by = models.ForeignKey(User, name='created_by')
+    date_created = models.DateTimeField(auto_now_ad2d=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, name='created_by')
     last_updated = models.DateTimeField(auto_now=True)
     content = models.TextField()
 
@@ -34,9 +40,9 @@ class Project(models.Model):
     description = models.TextField()
     date_created = models.DateField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
-    last_updated_by = models.ForeignKey(User, name='last_updated_by')
-    allowed_editors = models.ForeignKey(User, related_name='editors')
-    tasks = models.ForeignKey(Task)
-    notes = models.ForeignKey(Note)
+    last_updated_by = models.ForeignKey(User, on_delete=models.SET(dud_user), name='last_updated_by')
+    allowed_editors = models.ForeignKey(User, on_delete=models.SET(dud_user), related_name='editors')
+    tasks = models.ForeignKey(Task, on_delete=models.DO_NOTHING, name="todos")
+    notes = models.ForeignKey(Note, on_delete=models.CASCADE, name="project notes")
 
 
