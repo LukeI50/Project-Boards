421fa936891902c2057425f10e35900b92470b6e
LukeI50 2 <180131099+LukeI50@users.noreply.github.com>
2024-11-29 00:23:35 +0000

    Add: summernotes import and ProjectAdmin Summernotes app to admin.py

--- project_board/admin.py
+++ project_board/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import Project, Task, Note
+from django_summernote.admin import SummernoteModelAdmin
+
+
+class ProjectAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'slug', 'date_created')
+    search_fields = ['title']
+    list_filter = ('authorised_editors',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('description',)
+
 
 # Register your models here.
 admin.site.register(Project)
