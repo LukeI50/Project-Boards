adc231b123a5754268ee86813106cd0958c7affc
LukeI50 2 <180131099+LukeI50@users.noreply.github.com>
2024-11-29 15:54:01 +0000

    Add: Static_root var to settings.py and collect static files into root directory

--- config/settings.py
+++ config/settings.py
@@ -24,6 +24,8 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
