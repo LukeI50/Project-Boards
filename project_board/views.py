fdf3d52f929296d884db1554c57f8b864cd03176
LukeI50 2 <180131099+LukeI50@users.noreply.github.com>
2024-11-29 17:33:50 +0000

    Add: project_detail view function

--- project_board/views.py
+++ project_board/views.py
@@ -8,3 +8,27 @@ class ProjectsList(generic.ListView):
     queryset = Project.objects.all()
     template_name = 'project_board/index.html'
     paginate_by = 6
+
+
+def project_detail(request, slug):
+    """
+    Display an individual :model:`project_board.Project`
+
+    **Context**
+
+    ``project``
+        An instance of :model:`project_board.Project`.
+    
+    **Template**
+
+    :template:`project_board/project_detail.html`
+    """
+
+    queryset = Project.objects.all()
+    project = get_object_or_404(queryset, slug = slug)
+
+    return render(
+        request,
+        "project_board/project_detail.html",
+        {"project": project},
+    )
