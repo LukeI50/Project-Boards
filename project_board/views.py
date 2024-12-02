f16964e7d27a212029bb238b88d1d680b04f71fc
LukeI50 2 <180131099+LukeI50@users.noreply.github.com>
2024-12-02 20:14:25 +0000

    Add: form post processing to ProjectsList view class

--- project_board/views.py
+++ project_board/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import generic
 from .forms import NewProjectForm
 
@@ -6,6 +6,9 @@ from .models import Project
 
 # Create your views here.
 class ProjectsList(generic.ListView):
+    template_name = 'project_board/index.html'
+    paginate_by = 6
+
 
     def get_queryset(self):
         if self.request.user.is_anonymous:
@@ -15,9 +18,18 @@ class ProjectsList(generic.ListView):
 
     queryset = get_queryset
 
-    template_name = 'project_board/index.html'
-    paginate_by = 6
-
+    def post(self, request, *args, **kwargs):
+        form = NewProjectForm(request.POST)
+        if form.is_valid():
+            new_project = form.save(commit=False)
+            new_project.owner = request.user
+            new_project.last_edited_by = request.user
+            new_project.save()
+            return redirect('home')
+        else:
+            context = self.get_context_data()
+            context['project_form'] = form
+            return self.render_to_response(context)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data()
