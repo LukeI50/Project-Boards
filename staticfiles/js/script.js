913755c339ddf4e52867d009658864d49c0eaf25
LukeI50 2 <180131099+LukeI50@users.noreply.github.com>
2024-12-02 14:42:11 +0000

    Add: New Front page layout for unauthorised users

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -1,1 +1,8 @@
-console.log("Testing Static JS Script");
+document.addEventListener("DOMContentLoaded", () => {
+
+    // If the carousel element it present within DOM, then initialise the variable.
+    if (document.getElementById('myCarousel')) {
+        const myCarousel = bootstrap.Carousel.getInstance('#myCarousel');
+    }
+
+})
