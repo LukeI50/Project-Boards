474d053b91234e6495c5f9d27e9d76e5f4ab2d3f
LukeI50 2 <180131099+LukeI50@users.noreply.github.com>
2024-12-04 20:10:05 +0000

    refact: script.js resize listener to ensure refresh loop does not occur

--- static/js/script.js
+++ static/js/script.js
@@ -1,34 +1,31 @@
 // Ensure the DOM is fully loaded
 document.addEventListener("DOMContentLoaded", () => {
-    const breakpoints = [768, 1024]
+    // const breakpoints = [768, 1024]
+    const breakpoints = {default: 1024, small: 768};
+    console.log(`Breakpoints with word ${breakpoints['small']}`);
+
     const currentMode = document.cookie
         .split('; ')
         .find((row) => row.startsWith('currentMode='))
         ?.split("=")[1]
-
-    console.log(currentMode)
     
-    function diff(a, b) {return (a-b)}
-
     function setScreenValues(currentWidth, mode) {
         document.cookie = "screenWidth=" + `${currentWidth}` + "; path=/; Secure; SameSite=None";
         document.cookie = "currentMode=" + `${mode}` + "; path=/; Secure; SameSite=None";
         window.location.reload();
     }
-
+        
     window.addEventListener("resize", () => {
         let currentWidth = window.innerWidth;
+        console.log(currentMode);
 
-        if (diff(currentWidth, breakpoints[0]) < 0 && currentMode != 'small') {
-            // Set the cookie to the new screenWidth
+        console.log(currentWidth);
+
+        if (currentWidth < breakpoints.small && currentMode !== 'small') {
             setScreenValues(currentWidth, "small");
-
-        } else if (diff(currentWidth, breakpoints[1]) < 0 && currentMode != 'default') {
-            setScreenValues(currentWidth, "default");
-
+        } else if (currentWidth >= breakpoints.small && currentWidth < breakpoints.default && currentMode !== "default") {
+            setScreenValues(currentWidth, 'default');
         }
-
-
     })
 
 
