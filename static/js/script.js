2c9fd6ca080521f07b18d07a2870ce6860383811
LukeI50 2 <180131099+LukeI50@users.noreply.github.com>
2024-12-04 17:36:25 +0000

    Add: JavaScript to reload the page upon resizing of the window

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,13 @@
 // Ensure the DOM is fully loaded
 document.addEventListener("DOMContentLoaded", () => {
+    let resizeTimeout;
+
+    window.addEventListener("resize", () => {
+        clearTimeout(resizeTimeout); //Clear existing timeout
+        resizeTimeout = setTimeout(() => {
+            window.location.reload();
+        }, 200);
+    });
+
 
 });
