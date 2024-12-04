11718b139a1fc573fe1a9d579d2dac13fc8897af
LukeI50 2 <180131099+LukeI50@users.noreply.github.com>
2024-12-04 18:26:03 +0000

    Add: new JS code to utilise a delta to determine whether a reload is needed

--- static/js/script.js
+++ static/js/script.js
@@ -1,18 +1,26 @@
 // Ensure the DOM is fully loaded
 document.addEventListener("DOMContentLoaded", () => {
     let initialWidth = window.innerWidth;
+    const breakpoints = [768, 1024]
+    let currentMode = null
     
     function diff(a, b) {return Math.abs(a-b)}
 
     window.addEventListener("resize", () => {
-        let width = window.innerWidth;
-        
-        const breakpoints = (768, 1024)
+        let currentWidth = window.innerWidth;
 
-        if (diff(width, breakpoints[0])){
+        if (diff(currentWidth, breakpoints[1]) != true && currentMode != 'small') {
+            // set current mode to avoid refreshing within same breakpoint
+            currentMode = 'small'
+
+            // Set the cookie to the new screenWidth
             document.cookie = "screenWidth=" + window.innerWidth + "; path=/";
+
+            // Reload the window with the new screenWidth cookie value
             window.location.reload();
         }
+
+
     })
 
 
