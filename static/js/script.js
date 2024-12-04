b2034841d32db95835ae6f9969ff4bf5efd38d83
LukeI50 2 <180131099+LukeI50@users.noreply.github.com>
2024-12-04 18:09:38 +0000

    Add: prototype JS reload engine for screen dimensions

--- static/js/script.js
+++ static/js/script.js
@@ -1,13 +1,19 @@
 // Ensure the DOM is fully loaded
 document.addEventListener("DOMContentLoaded", () => {
-    let resizeTimeout;
+    let initialWidth = window.innerWidth;
+    
+    function diff(a, b) {return Math.abs(a-b)}
 
     window.addEventListener("resize", () => {
-        clearTimeout(resizeTimeout); //Clear existing timeout
-        resizeTimeout = setTimeout(() => {
+        let width = window.innerWidth;
+        
+        const breakpoints = (768, 1024)
+
+        if (diff(width, breakpoints[0])){
+            document.cookie = "screenWidth=" + window.innerWidth + "; path=/";
             window.location.reload();
-        }, 200);
-    });
+        }
+    })
 
 
 });
