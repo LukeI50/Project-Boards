ef37c3a372d2862a8e1986b98e13622f4231784b
LukeI50 2 <180131099+LukeI50@users.noreply.github.com>
2024-12-04 20:24:36 +0000

    Add: comments to script.js to make it readable

--- static/js/script.js
+++ static/js/script.js
@@ -1,32 +1,33 @@
 // Ensure the DOM is fully loaded
 document.addEventListener("DOMContentLoaded", () => {
+
     // const breakpoints = [768, 1024]
     const breakpoints = {default: 1024, small: 768};
-    console.log(`Breakpoints with word ${breakpoints['small']}`);
 
+    // Get the value stored in the currentMode cookie
     const currentMode = document.cookie
         .split('; ')
         .find((row) => row.startsWith('currentMode='))
-        ?.split("=")[1]
+        ?.split("=")[1];
     
+    // set the cookie values for the screen width and mode
     function setScreenValues(currentWidth, mode) {
         document.cookie = "screenWidth=" + `${currentWidth}` + "; path=/; Secure; SameSite=None";
         document.cookie = "currentMode=" + `${mode}` + "; path=/; Secure; SameSite=None";
         window.location.reload();
     }
-        
+
+
+    
     window.addEventListener("resize", () => {
-        let currentWidth = window.innerWidth;
-        console.log(currentMode);
+        const currentWidth = window.innerWidth;
 
-        console.log(currentWidth);
-
+        // Set screen width logic
         if (currentWidth < breakpoints.small && currentMode !== 'small') {
             setScreenValues(currentWidth, "small");
         } else if (currentWidth >= breakpoints.small && currentWidth < breakpoints.default && currentMode !== "default") {
             setScreenValues(currentWidth, 'default');
-        }
+        } 
     })
-
 
 });
