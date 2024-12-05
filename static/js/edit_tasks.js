08b9f49594eae92d6b602b2e209517097fe93261
LukeI50 2 <180131099+LukeI50@users.noreply.github.com>
2024-12-05 18:41:38 +0000

    Add: DOMcontentloaded event for edit_tasks.js

--- static/js/edit_tasks.js
+++ static/js/edit_tasks.js
@@ -1,27 +1,31 @@
-const editButtons = document.getElementsByClassName("btn-task-edit");
-const taskText = document.getElementById("id_body");
-const taskForm = document.getElementById("taskForm");
-const submitButton = document.getElementById("submitButton");
-console.log("task edit script successfully loaded")
-/**
- * Initialises edit functionality for the provided edit buttons.
- * 
- * For each button in the `editButtons` collection:
- * - Retrieves the associated task's ID upon
- * click.
- * - Fetches the content of the corresponding task.
- * - Populates the `taskContent` input/textarea with 
- * the task's content for editing.
- * - Updates the submit button's text to "Update".
- * - Sets the form's action attribute to the `edit_comment/{commentId}` endpoint.
- */
-for (let button of editButtons){
-    console.log(editButtons)
-    button.addEventListener("click", (e) => {
-        let taskId = e.target.getAttribute("task_id");
-        let taskContent = document.getElementById(`task${taskId}`).innerText;
-        taskText.value = taskContent;
-        submitButton.innerText = "Update";
-        taskForm.setAttribute("action", `edit_comment/${commentId}`);
-    })
-}
+document.addEventListener("DOMContentLoaded", () => {
+    
+    const editButtons = document.getElementsByClassName("btn-task-edit");
+    const taskText = document.getElementById("id_body");
+    const taskForm = document.getElementById("taskForm");
+    const submitButton = document.getElementById("submitButton");
+    console.log("task edit script successfully loaded")
+    /**
+     * Initialises edit functionality for the provided edit buttons.
+     * 
+     * For each button in the `editButtons` collection:
+     * - Retrieves the associated task's ID upon
+     * click.
+     * - Fetches the content of the corresponding task.
+     * - Populates the `taskContent` input/textarea with 
+     * the task's content for editing.
+     * - Updates the submit button's text to "Update".
+     * - Sets the form's action attribute to the `edit_comment/{commentId}` endpoint.
+     */
+    for (let button of editButtons){
+        console.log(editButtons)
+        button.addEventListener("click", (e) => {
+            let taskId = e.target.getAttribute("task_id");
+            let taskContent = document.getElementById(`task${taskId}`).innerText;
+            taskText.value = taskContent;
+            submitButton.innerText = "Update";
+            taskForm.setAttribute("action", `edit_comment/${commentId}`);
+        })
+    }
+
+})
