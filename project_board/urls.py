e4d95ecce1c58dce3fb880bf4d7b64ef5f406ddd
LukeI50 2 <180131099+LukeI50@users.noreply.github.com>
2024-12-06 01:13:21 +0000

    Add: urls.py delete_task path with slug and task_id being passed in url

--- project_board/urls.py
+++ project_board/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('collaborator/', views.CollaboratorList.as_view(), name='collaborator'),
     path('<slug:slug>/', views.ProjectDetailView.as_view(), name = 'project_detail'),
+    path('<slug:slug>/delete_task/<int:task_id>', views.task_delete, name='task_delete'),
     path('<slug:slug>/edit_task/<int:task_id>', views.task_edit, name='task_edit'),
     path('', views.ProjectsList.as_view(), name='home'),
 ]
