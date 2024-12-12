6f2beda169686f0937df76c47340e96a279f77ce
LukeI50 <180131099+LukeI50@users.noreply.github.com>
2024-12-12 08:05:53 +0000

    add: docstrings to classes in project_board forms.py file

--- project_board/forms.py
+++ project_board/forms.py
@@ -3,6 +3,13 @@ from django import forms
 
 
 class NewProjectForm(forms.ModelForm):
+    """
+    Form for creating a new project.
+
+    This form is used to collect data from users for creating a new project.
+    It includes fields for the project title and description.
+    """
+    
     class Meta:
         model = Project
         fields = (
@@ -12,6 +19,13 @@ class NewProjectForm(forms.ModelForm):
 
 
 class NewTaskForm(forms.ModelForm):
+    """
+    Form for creating a new task.
+
+    This form is used to collect data from users for creating a new task.
+    It includes fields for the task title, content, and status.
+    """
+
     class Meta:
         model = Task
         fields = (
@@ -22,6 +36,13 @@ class NewTaskForm(forms.ModelForm):
 
 
 class EditShortNotes(forms.ModelForm):
+    """
+    Form for editing short notes.
+
+    This form is used to collect data from users for editing the short content of a note.
+    It includes a single field for the short note content.
+    """
+
     class Meta:
         model = Note
         fields = (
