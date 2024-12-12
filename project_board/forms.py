988e62ae0f02407960a7f13979c1c53b6c79788f
LukeI50 <180131099+LukeI50@users.noreply.github.com>
2024-12-12 08:33:31 +0000

    refactor: docstrings in forms.py

--- project_board/forms.py
+++ project_board/forms.py
@@ -8,8 +8,13 @@ class NewProjectForm(forms.ModelForm):
 
     This form is used to collect data from users for creating a new project.
     It includes fields for the project title and description.
+
+    Attributes:
+        model (Model): The Django model associated with this form, which is the `Project` model.
+        fields (tuple): A tuple of field names that should be included in the form. In this case,
+                         it includes 'title' and 'description'.
     """
-    
+
     class Meta:
         model = Project
         fields = (
@@ -24,6 +29,11 @@ class NewTaskForm(forms.ModelForm):
 
     This form is used to collect data from users for creating a new task.
     It includes fields for the task title, content, and status.
+
+    Attributes:
+        model (Model): The Django model associated with this form, which is the `Task` model.
+        fields (tuple): A tuple of field names that should be included in the form. In this case,
+                         it includes 'taskTitle', 'content', and 'status'.
     """
 
     class Meta:
@@ -41,6 +51,11 @@ class EditShortNotes(forms.ModelForm):
 
     This form is used to collect data from users for editing the short content of a note.
     It includes a single field for the short note content.
+
+    Attributes:
+        model (Model): The Django model associated with this form, which is the `Note` model.
+        fields (tuple): A tuple of field names that should be included in the form. In this case,
+                         it includes 'short'.
     """
 
     class Meta:
