89d458f0e7e0f14d70677392d88bd704788d25cf
LukeI50 2 <180131099+LukeI50@users.noreply.github.com>
2024-12-12 05:12:14 +0000

    fix: python linter errors in about apps python files

--- about/models.py
+++ about/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class About(models.Model):
     title = models.CharField(max_length=200)
@@ -8,4 +9,3 @@ class About(models.Model):
 
     def __str__(self):
         return self.title
-    
